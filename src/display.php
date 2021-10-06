b9028e4cb42241460508acbbd9605b12eb15afab
kapptK <kapptK@gmail.com>
2021-10-06 15:48:28 -0400

    updated display

--- src/display.php
+++ src/display.php
@@ -4,21 +4,21 @@ $host = 'assignment3-database-mysql.ctr5gqfznkbs.us-east-2.rds.amazonaws.com';
 $username = 'x12124';
 $password = 'miniclips';
 $db_name = 'assignment3-database-mysql'
-$conn = mysql_init();
-mysql_real_connect($conn, $host, $username, $password, $db_name, 3306);
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 
-if(mysql_connect_errno($conn)) {
+if(mysqli_connect_errno($conn)) {
 	die('Failed to connect to mySQL: ');
 }
 
 $sql = 'SELECT * FROM Students';
-$result = mysql_query($conn, $sql);
+$result = mysqli_query($conn, $sql);
 
-while($row = mysql_fetch_assoc($result)) {
+while($row = mysqli_fetch_assoc($result)) {
 	echo"database says {$row} <br>";
 }
 
-mysql_close($conn);
+mysqli_close($conn);
 
 ?>
 <h1>hello</h1>
